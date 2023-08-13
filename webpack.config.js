50041d85d1e8a6fb740d292601905a0a2ec13c92
Bair <73276298+zoxione@users.noreply.github.com>
2023-08-13 11:30:46 +0300

    update dependencies and fixes

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,42 @@
-const path = require('path');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: 'production',
-  entry: './src/index.js',
+  mode: "production",
+  entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
   },
   module: {
     rules: [
       {
         test: /\.js$/i,
-        include: path.resolve(__dirname, 'src'),
+        include: path.resolve(__dirname, "src"),
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-env'],
+            presets: ["@babel/preset-env"],
           },
         },
       },
       {
         test: /\.css$/i,
-        include: path.resolve(__dirname, 'src'),
-        use: ['style-loader', 'css-loader', 'postcss-loader'],
+        include: path.resolve(__dirname, "src"),
+        use: ["style-loader", "css-loader", "postcss-loader"],
       },
     ],
   },
   devServer: {
-    contentBase: path.resolve(__dirname, 'dist'),
-    watchContentBase: true,
+    static: path.resolve(__dirname, "src"),
+    port: 8080,
+    open: true,
+    hot: true,
   },
   plugins: [
     new HTMLWebpackPlugin({
-      template: './src/index.html',
-      favicon: './src/favicon.svg',
-    })
+      template: "./src/index.html",
+      favicon: "./src/favicon.ico",
+    }),
   ],
 };
